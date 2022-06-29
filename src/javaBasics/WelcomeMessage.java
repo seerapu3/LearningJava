569599b529827285bc19edfee956e839a805a3ca
seerapu3 <seerapuqaengineer@outlook.com>
2022-06-29 17:57:44 +0530

    from windows

--- src/javaBasics/WelcomeMessage.java
+++ src/javaBasics/WelcomeMessage.java
@@ -5,5 +5,6 @@ public class WelcomeMessage {
 	public static void main(String[] args) {
 		System.out.println("Welcome to learn Java from from windows");
 		System.out.println("Finaly i made it");
+		System.out.println("Again cloned");
 	}
 }
