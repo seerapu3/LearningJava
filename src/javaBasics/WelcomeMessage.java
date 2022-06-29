29909d62bc8cc953857539468ddaa889ca249c3a
Seerapu <seerapu@192.168.1.10>
2022-06-29 17:48:08 +0530

    finally one more line

--- src/javaBasics/WelcomeMessage.java
+++ src/javaBasics/WelcomeMessage.java
@@ -4,5 +4,6 @@ public class WelcomeMessage {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome to learn Java from from windows");
+		System.out.println("Finaly i made it");
 	}
 }
