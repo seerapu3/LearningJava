0d513e947284339da85b46231b61b1e8eb9230c1
Seerapu <seerapu@Seerapus-MacBook-Air.local>
2022-07-01 19:14:06 +0530

    modified loops package

--- src/loops/ForEachLoops.java
+++ src/loops/ForEachLoops.java
@@ -6,10 +6,19 @@ public class ForEachLoops {
 		
 		String[] cars = {"Volvo", "BMW", "Ford", "Mazda"};
 
-		for(String i:cars)
+		for(String str:cars)
 		{
-			System.out.println(i);
+			System.out.println(str);
 		}
+		
+		
+		int[] i = {10,20,30,40,50};
+		for(int j:i)
+		{
+			System.out.println(j);
+		}
+		
+		
 	}
 
 }
